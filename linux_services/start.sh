16793c5301a8b1c06ef924899714167ba4c97cce
FredJ21 <frederic@jelmoni.fr>
2024-01-20 13:08:08 +0100

    Add Linux services

---
+++ linux_services/start.sh
@@ -0,0 +1,9 @@
+#------------------------------------------------------------------------------
+#							MINION services
+#													Fred J.		Janvier 2024
+#------------------------------------------------------------------------------
+
+clear
+sudo systemctl start minion_core
+sudo systemctl start minion_web
+sudo systemctl start minion_cam
---
+++ linux_services/stop.sh
@@ -0,0 +1,9 @@
+#------------------------------------------------------------------------------
+#							MINION services
+#													Fred J.		Janvier 2024
+#------------------------------------------------------------------------------
+
+clear
+sudo systemctl status minion_core
+sudo systemctl status minion_web
+sudo systemctl status minion_cam

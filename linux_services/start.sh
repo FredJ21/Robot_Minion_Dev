57b36c04b9dc3863abbe639672bd77cc30117a6a
fred <fred>
2024-01-20 14:44:51 +0100

    update services

--- linux_services/start.sh
+++ linux_services/start.sh
@@ -2,7 +2,7 @@
 #							MINION services
 #													Fred J.		Janvier 2024
 #------------------------------------------------------------------------------
-
+set -x
 
 sudo systemctl start minion_core
 sudo systemctl start minion_web
--- linux_services/stop.sh
+++ linux_services/stop.sh
@@ -2,7 +2,7 @@
 #							MINION services
 #													Fred J.		Janvier 2024
 #------------------------------------------------------------------------------
-
+set -x
 
 sudo systemctl stop minion_core
 sudo systemctl stop minion_web

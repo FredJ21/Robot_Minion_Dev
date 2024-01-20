53fbebccde1de1cb6d35e69b30826738b208d204
FredJ21 <frederic@jelmoni.fr>
2024-01-20 13:05:40 +0100

    Add Linux services

--- linux_services/install_service_minion.sh
+++ linux_services/install_service_minion.sh
@@ -3,6 +3,14 @@
 #													Fred J.		Janvier 2024
 #------------------------------------------------------------------------------
 
+sudo systemctl stop minion_core
+sudo systemctl stop minion_web
+sudo systemctl stop minion_cam
+
+sudo systemctl disable minion_core
+sudo systemctl disable minion_web
+sudo systemctl disable minion_cam
+
 
 sudo cp *.service /etc/systemd/system
 
